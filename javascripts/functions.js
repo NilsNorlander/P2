49f8e94133a6945c97c0f3f17f707c8d0016bcfc
Nils Norlander <NilsN@128-223-223-233-wireless.uoregon.edu>
2015-04-13 10:17:50 -0700

    Edited Index and faq.html

---
+++ javascripts/functions.js
@@ -0,0 +1,12 @@
+var isArmstrong(n){
+	"use strict"; var sum = 0;
+	n = String(n);
+	//traverse n
+	for(var i = 0; i<n.length; ++i)
+		sum = sum+Math.pow(Number(n[i]), n.length);
+	if(n==sum)
+		return true
+	else
+		return false
+
+}
