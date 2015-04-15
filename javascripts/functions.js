29b0a56d938f39efd99de56416c0949aa459dfac
Nils Norlander <NilsN@128-223-223-232-wireless.uoregon.edu>
2015-04-15 08:33:33 -0700

    Changed styles in style.css and edited index/faq.

--- javascripts/functions.js
+++ javascripts/functions.js
@@ -11,3 +11,52 @@ function isArmstrong(n){
 		
 	return false
 };
+
+function allArmstrongs(){
+	var result = "";
+	//generate
+	for (var k=1; k<=99999; ++k){
+		//test
+		if (isArmstrong(k)){
+			result += String(k) + ' ';
+
+		}
+	}
+	return result
+};
+
+function allSubstrings1(str){
+	result=''; word='';
+	for(var k=0; k<str.length; ++k){
+	for (var i=k;  i<str.length; ++i) {
+		word += str[i];
+		result += word + ',';
+	}
+	word = '';
+}
+	return result;
+};
+
+function allSubstrings2(str){
+	result=[]; word='';
+	for(var k=0; k<str.length; ++k){
+	for (var i=k;  i<str.length; ++i) {
+		word += str[i];
+		result.push(word)
+	}
+	word = '';
+}
+	return result;
+};
+
+function maxWord(str){
+	str = str.split(" ");
+	result = '';
+	for(i=0; i<str.length; ++i){
+		if(str[i].length > result.length){
+			result = str[i];
+		};
+	};
+	return result
+};
+
